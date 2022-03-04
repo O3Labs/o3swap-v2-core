f99b5cbd1e0e1d22b8a65c7ca40227cf3f32b4a7
Rick Cain <90382366+rickcain@users.noreply.github.com>
2022-03-04 11:48:27 +0800

    chore: update truffle config

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,15 +2,5 @@ const Pool = artifacts.require("Pool");
 const PToken = artifacts.require("PToken");
 
 module.exports = function (deployer, network, accounts) {
-    switch (network) {
-        case "development_pool":
-            // Fill required parameters before deploy.
-            deployer.deploy(Pool);
-            break;
 
-        case "development_ptoken":
-            // Fill required parameters before deploy.
-            deployer.deploy(PToken);
-            break;
-    }
 }
